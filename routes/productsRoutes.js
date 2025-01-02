9c00968efb5038802e12d70de6150a5ea04a88c0
Sai Kiran Reddy Mothe <kiran2712reddy@gmail.com>
2025-01-02 11:10:48 -0500

    first commit

---
+++ routes/productsRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import auth from '../middlewares/auth.js'
+const app = express()
+import {
+  addProduct,
+  removeProduct,
+  showProducts,
+  updateProduct,
+} from "../controllers/productsController.js";
+const productRouter = express.Router();
+
+app.use(auth)
+
+productRouter.get("/all", showProducts);
+productRouter.post("/add", addProduct);
+productRouter.patch("/update/:pid", updateProduct);
+productRouter.delete("/delete/:pid", removeProduct);
+export default productRouter;
